1b1d49d78a36b0c119e08cf98f95fbc2bbf2306c
‘juanclement’ <juanclement_@live.com>
2023-06-17 14:35:55 -0500

    ajustes app node conexion a bd rds

--- app.js
+++ app.js
@@ -3,33 +3,33 @@ const mysql = require('mysql2');
 
 const app = express();
 const port = 3000;
-// const connection = mysql.createConnection({
-//   host: 'bdpruebadesapublicrdwauropublic.cluster-cptsd6gduasw.us-east-1.rds.amazonaws.com',
-//   port: 3306,
-//   user: 'admin',
-//   password: 'admin1234.',
-//   database: 'bdpruebadesapublicrdwauropublic'
-// });
+const connection = mysql.createConnection({
+  host: 'bdtargencar.cluster-ro-cptsd6gduasw.us-east-1.rds.amazonaws.com',
+  port: 3306,
+  user: 'admin',
+  password: 'admin1234.',
+  database: 'holamund'
+});
 
-// connection.connect((error) => {
-//   if (error) {
-//     console.error('Error al conectar a la base de datos:', error);
-//   } else {
-//     console.log('Conexión exitosa a la base de datos');
+connection.connect((error) => {
+  if (error) {
+    console.error('Error al conectar a la base de datos:', error);
+  } else {
+    console.log('Conexión exitosa a la base de datos');
 
-//     // Realiza una consulta a la base de datos
-//     // connection.query('SELECT * FROM tabla', (error, results) => {
-//     //   if (error) {
-//     //     console.error('Error al realizar la consulta:', error);
-//     //   } else {
-//     //     console.log('Resultados de la consulta:', results);
-//     //   }
+    //Realiza una consulta a la base de datos
+    connection.query('SELECT * FROM marcas_carros', (error, results) => {
+      if (error) {
+        console.error('Error al realizar la consulta:', error);
+      } else {
+        console.log('Resultados de la consulta:', results);
+      }
 
-//     //   // Cierra la conexión a la base de datos
-//     //   connection.end();
-//     // });
-//   }
-// });
+      // Cierra la conexión a la base de datos
+      connection.end();
+    });
+  }
+});
 
 
 app.get('/', (req, res) => {
